afde9d1ee256ee962e6ecfa9372ea27a54968d8b
0xddi <7778885@bk.ru>
2026-02-28 13:25:11 +0500

    initial commit

---
+++ Practice3.Tests/CheckerBoardPositionTest.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Globalization;
+using ChessExample;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace Practice3.Tests;
+
+[TestSubject(typeof(CheckerBoardPosition))]
+public class CheckerBoardPositionTests
+{
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(8, 8)]
+    [InlineData(4, 5)]
+    [InlineData(1, 8)]
+    [InlineData(8, 1)]
+    public void Constructor_PassValidCoordinates_Success(byte x, byte y)
+    {
+        // Act
+        var position = new CheckerBoardPosition(x, y);
+        
+        // Assert
+        Assert.Equal(x, position.X);
+        Assert.Equal(y, position.Y);
+    }
+    
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(9, 1)]
+    [InlineData(1, 0)]
+    [InlineData(1, 9)]
+    [InlineData(0, 0)]
+    [InlineData(9, 9)]
+    public void Constructor_PassInvalidCoordinates_ThrowsArgumentOutOfRangeException(byte x, byte y)
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentOutOfRangeException>(() => new CheckerBoardPosition(x, y));
+    }
+    
+    [Theory]
+    [InlineData("khzjkhhjeihgiw")]
+    [InlineData("TeSSt081k;sl")]
+    [InlineData("!@#::$")]
+    [InlineData("")]
+    [InlineData(null)]
+    public void Parse_InvalidString_ThrowsFormatException(string invalidString)
+    {
+        // Act & Assert
+        Assert.Throws<FormatException>(() => 
+            CheckerBoardPosition.Parse(invalidString, CultureInfo.InvariantCulture));
+    }
+    
+    
+    [Theory]
+    [InlineData(1, 'A')]
+    [InlineData(2, 'B')]
+    [InlineData(3, 'C')]
+    [InlineData(4, 'D')]
+    [InlineData(5, 'E')]
+    [InlineData(6, 'F')]
+    [InlineData(7, 'G')]
+    [InlineData(8, 'H')]
+    public void Constructor_PassValidNumberForConvertingToLetter_Success(byte x, char expectedLetter)
+    {
+        // Arrange
+        var position = new CheckerBoardPosition(x, 1);
+        
+        // Act
+        var letter = position.XLetter;
+        
+        // Assert
+        Assert.Equal(expectedLetter, letter);
+    }
+    
+    [Theory]
+    [InlineData(1, 1, "A1")]
+    [InlineData(8, 8, "H8")]
+    [InlineData(4, 5, "D5")]
+    [InlineData(2, 7, "B7")]
+    public void ToString_ValidPosition_Success(byte x, byte y, string expected)
+    {
+        // Arrange
+        var position = new CheckerBoardPosition(x, y);
+        
+        // Act
+        var result = position.ToString();
+        
+        // Assert
+        Assert.Equal(expected, result);
+    }
+    
+    
+    [Theory]
+    [InlineData("A1", true)]
+    [InlineData("Z9", false)]
+    [InlineData(null, false)]
+    // Можно ли в одном и том же методе теста проверять верные и неверные кейсы?
+    // Или обязательны отдельные методы с ..._Success и ..._Fail?
+    public void TryParse_InvalidAndValidInputs_ReturnsExpectedResult(string input, bool expectedSuccess)
+    {
+        // Act
+        var success = CheckerBoardPosition.TryParse(input, CultureInfo.InvariantCulture, out var position);
+        
+        // Assert
+        Assert.Equal(expectedSuccess, success);
+    }
+    
+}
---
+++ Practice3/Program.cs
@@ -0,0 +1,155 @@
+﻿using System.Globalization;
+using ChessExample;
+
+namespace Practice3;
+
+public enum ColorEnum
+{
+    White = 1,
+    Black
+}
+
+abstract class ChessFigure
+{
+    protected CheckerBoardPosition StartPosition;
+    protected CheckerBoardPosition EndPosition;
+    
+    public abstract bool IsAbleToMove();
+
+    public ChessFigure(CheckerBoardPosition startPosition, CheckerBoardPosition endPosition)
+    {
+        this.StartPosition = startPosition;
+        this.EndPosition = endPosition;
+    }
+}
+
+class Pawn : ChessFigure
+{
+    protected int Color;
+
+    private bool CheckColor(CheckerBoardPosition position, int color)
+    {
+        if ((Math.Abs(position.X - position.Y) % 2 == 0) && color == (int)ColorEnum.Black) return true;
+        if ((Math.Abs(position.X - position.Y) % 2 == 1) && color == (int)ColorEnum.White) return true;
+        return false;
+    }
+    
+    public override bool IsAbleToMove()
+    {
+        return (Math.Abs(StartPosition.X - EndPosition.X) == 1) && (Math.Abs(StartPosition.Y - EndPosition.Y) == 1) && CheckColor(StartPosition, Color) && CheckColor(EndPosition, Color);
+    }
+
+    public Pawn(CheckerBoardPosition startPosition, CheckerBoardPosition endPosition, int color) : base(startPosition, endPosition)
+    {
+        this.Color = color; 
+    }
+}
+
+class Rook : ChessFigure
+{
+    public override bool IsAbleToMove()
+    {
+        return (StartPosition.X == EndPosition.X) || (StartPosition.Y == EndPosition.Y);
+    }
+
+    public Rook(CheckerBoardPosition startPosition, CheckerBoardPosition endPosition) : base(startPosition, endPosition) {}
+}
+
+class Knight : ChessFigure
+{
+    public override bool IsAbleToMove()
+    {
+        return (Math.Abs(StartPosition.X - EndPosition.X) == 1) && (Math.Abs(StartPosition.Y - EndPosition.Y) == 2) || ((Math.Abs(StartPosition.X - EndPosition.X) == 2) && (Math.Abs(StartPosition.Y - EndPosition.Y) == 1));
+    }
+
+    public Knight(CheckerBoardPosition startPosition, CheckerBoardPosition endPosition) : base(startPosition, endPosition) {}
+}
+
+class Bishop : ChessFigure
+{
+    public override bool IsAbleToMove()
+    {
+        return Math.Abs(StartPosition.X - EndPosition.X) == Math.Abs(StartPosition.Y - EndPosition.Y);
+    }
+
+    public Bishop(CheckerBoardPosition startPosition, CheckerBoardPosition endPosition) : base(startPosition, endPosition) {}
+}
+
+class Queen : ChessFigure
+{
+    public override bool IsAbleToMove()
+    {
+        return (StartPosition.X == EndPosition.X || StartPosition.Y == EndPosition.Y) || Math.Abs(StartPosition.X - EndPosition.X) == Math.Abs(StartPosition.Y - EndPosition.Y);
+    }
+    
+    public Queen(CheckerBoardPosition startPosition, CheckerBoardPosition endPosition) : base(startPosition, endPosition)
+    {
+        this.StartPosition = startPosition;
+        this.EndPosition = endPosition;
+    }
+}
+
+class King : ChessFigure
+{
+    public override bool IsAbleToMove()
+    {
+        return (Math.Abs(StartPosition.X - EndPosition.X) <= 1) && (Math.Abs(StartPosition.Y - EndPosition.Y) <= 1);
+    }
+    
+    public King(CheckerBoardPosition startPosition, CheckerBoardPosition endPosition) : base(startPosition, endPosition)
+    {
+        this.StartPosition = startPosition;
+        this.EndPosition = endPosition;
+    }
+}
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the start position of the figure: ");
+        var inputStartPosition = CheckerBoardPosition.Parse(Console.ReadLine().ToUpper(), CultureInfo.InvariantCulture);
+        Console.Write("Enter the end position of the figure: ");
+        var inputEndPosition = CheckerBoardPosition.Parse(Console.ReadLine().ToUpper(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("Now choose your chess figure:");
+        Console.WriteLine("=============================");
+        Console.WriteLine("1. Black Pawn");
+        Console.WriteLine("2. White Pawn");
+        Console.WriteLine("3. Rook");
+        Console.WriteLine("4. Knight");
+        Console.WriteLine("5. Bishop");
+        Console.WriteLine("6. Queen");
+        Console.WriteLine("7. King");
+        Console.WriteLine("=============================");
+        Console.Write("Your choice: ");
+        var chosenFigure = int.Parse(Console.ReadLine());
+        switch (chosenFigure)
+        {
+            case 1:
+                Console.WriteLine((new Pawn(inputStartPosition, inputEndPosition, (int)ColorEnum.Black)).IsAbleToMove());
+                break;
+            case 2:
+                Console.WriteLine((new Pawn(inputStartPosition, inputEndPosition, (int)ColorEnum.White)).IsAbleToMove());
+                break;
+            case 3:
+                Console.WriteLine((new Rook(inputStartPosition, inputEndPosition)).IsAbleToMove());
+                break;
+            case 4:
+                Console.WriteLine((new Knight(inputStartPosition, inputEndPosition)).IsAbleToMove());
+                break;
+            case 5:
+                Console.WriteLine((new Bishop(inputStartPosition, inputEndPosition)).IsAbleToMove());
+                break;
+            case 6:
+                Console.WriteLine((new Queen(inputStartPosition, inputEndPosition)).IsAbleToMove());
+                break;
+            case 7:
+                Console.WriteLine((new King(inputStartPosition, inputEndPosition)).IsAbleToMove());
+                break;
+        }
+    }
+}
